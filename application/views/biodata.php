4d6fa304f92c8c8b72c5706ed672d2a3518c48f2
Fariz Ardin Adhiyaksa <fariz.ardin@gmail.com>
2019-03-16 17:41:22 +0700

    tambah pbkk

---
+++ application/views/biodata.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Biodata</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+	<link rel="stylesheet" href="http://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="<?php echo base_url("animate/animate.css") ?>">
+	<script src="<?php echo base_url("assets/js/jquery.js")?>"></script>
+	<script src="http://code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+	<script src="http://code.jquery.com/jquery-1.10.2.js"></script>
+</head>
+<body style="padding-top:2rem; padding-bottom:2rem; color:#686868;"> 
+ 
+<div class="container" style="width:100%; height:100%">
+  <div class="card" style="border-radius:0px !important; border-color:transparent !important; width:100% !important; height:100% !important; min-width:50rem;">
+    <div class="card-body">
+		<div class="row animated fadeInDown delay-0s">
+			<div class="col" style="max-width:12rem !important;">
+				<h2 style="padding-left:1rem">BIODATA</h2>
+			</div>
+			<div class="col" style="padding-top:1rem;">
+				<div style="height:5px; background-color:#ffd800;"></div>
+			</div>
+		</div>
+		<div class="row" style="padding-top:1rem;padding-bottom:0.25rem;">
+		<div class="col-4">
+		<img class="animated fadeInLeft delay-0s" src="<?php echo base_url("img/profil.jpg") ?>" alt="" style="width:70%; margin-left:3rem; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">
+			</div>
+			<div class="col animated fadeIn delay-0s slower">
+				<div class="row">
+					<div class="col-2">
+					Nama
+					</div>
+					<div class="col">
+					Fariz Ardin Adhiyaksa
+					</div>
+				</div>
+				<div class="row" style="padding-top:0.3rem;">
+					<div class="col-2">
+					NRP
+					</div>
+					<div class="col">
+					05111640000158
+					</div>
+				</div>
+				<div class="row" style="margin-top:2rem;">
+					<div class="col" style="max-width:8.5rem !important;">
+						Contact me at
+					</div>
+					<div class="col" style="margin-top:0.75rem;">
+					</div>
+				</div>
+				<div class="row" style="padding-top:1rem;">
+					<div class="col-1" style="max-width:8.5rem !important;">
+					<img src="<?php echo base_url("img/phone.png") ?>" alt="" style="width:15px; margin-left:1rem;">
+					</div>
+					<div class="col">
+						082238468283<img src="<?php echo base_url("img/wa.png") ?>" alt="" style="width:25px; padding-left:0.5rem; margin-top:-4px;">
+					</div>
+				</div>
+				<div class="row" style="margin-top:2rem;">
+					<div class="col-1" style="max-width:8.5rem !important;">
+					<a target="_blank" href="https://www.facebook.com/fariz.ardinadhiyaksa"><img src="<?php echo base_url("img/facebook.png") ?>" alt="" style="width:50px;"></a>
+					</div>
+					<div class="col-1" style="max-width:8.5rem !important;">
+					<a target="_blank"href="https://twitter.com/RizuuZu"><img src="<?php echo base_url("img/twitter.png") ?>" alt="" style="width:50px;"></a>
+					</div>
+					<div class="col-1" style="max-width:8.5rem !important;">
+					<a target="_blank" href="http://line.me/ti/p/~farizardin"><img src="<?php echo base_url("img/line.png") ?>" alt="" style="width:50px;"></a>
+					</div>
+					<div class="col-1" style="max-width:8.5rem !important;">
+					<a target="_blank" href="https://www.instagram.com/farizardin/"><img src="<?php echo base_url("img/ig.png") ?>" alt="" style="width:50px;"></a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row animated fadeInDown delay-1s" style="margin-top:2rem;">
+		<div class="col" style="padding-top:1rem;">
+				<div style="height:5px; background-color:#00a5ff;"></div>
+			</div>
+			<div class="col" style="max-width:12rem !important;">
+				<h2 style="padding-left:1rem">PROJECT</h2>
+			</div>
+			<div class="col" style="padding-top:1rem;">
+				<div style="height:5px; background-color:#00a5ff;"></div>
+			</div>
+		</div>
+		<div class="col animated fadeIn delay-1s slower">
+			<div class="row" style="margin-left:2rem;margin-top:2rem;">
+				<div class="col-3">
+				<img src="<?php echo base_url("img/simkwu.png") ?>" alt="" style="width:50%; margin-left:3rem;">
+				</div>
+				<div class="col">
+					<h3>SIMKWU</h3>
+					<p>SIMKWU adalah website yang membantu mahasiswa ITS dalam berwirausaha</p>
+				</div>
+			</div>
+			<div style="height:1px; background-color:#e3e3e3; margin-top:2rem !important; width:80%;margin:auto;" ></div>
+			<div class="row" style="margin-left:2rem;margin-top:2rem;">
+				<div class="col-3">
+				<img src="<?php echo base_url("img/pasar.png") ?>" alt="" style="width:50%; margin-left:3rem;">
+				</div>
+				<div class="col">
+					<h3>Sistem Reservasi Pasar Daerah Surabaya (PD. Pasar Surya)</h3>
+					<p>Sistem Reservasi Pasar Daerah Surabaya adalah sistem informasi untuk membantu pedagang dalam melakukan reservasi stand dan perizinan dalam usaha</p>
+				</div>
+			</div>
+			<div style="height:1px; background-color:#e3e3e3; margin-top:2rem !important; width:80%;margin:auto;" ></div>
+		</div>
+	</div>
+  </div>
+</div>
+
+</body>
+</html>
